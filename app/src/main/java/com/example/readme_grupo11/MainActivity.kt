afad600f4d2e0806e073af8008244c8d71bdaa1a
unknown <Cetecom@SJAPCA03LC0709.duoc.acad>
2025-10-10 12:06:59 -0300

    Inicializacion del proyecto

---
+++ app/src/main/java/com/example/readme_grupo11/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.readme_grupo11
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.readme_grupo11.ui.theme.README_Grupo11Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            README_Grupo11Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    README_Grupo11Theme {
+        Greeting("Android")
+    }
+}
