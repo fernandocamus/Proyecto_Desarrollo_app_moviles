8f8b2b00145e13c137904a8c22d931af554a0ad8
Sebastian Pino <se.pinob@duocuc.cl>
2025-10-11 14:26:50 -0300

    codigo de prueba para aprender a disrniar apps. No se aplican los tamaños de pantalla

--- app/src/main/java/com/example/readme_grupo11/MainActivity.kt
+++ app/src/main/java/com/example/readme_grupo11/MainActivity.kt
@@ -1,15 +1,30 @@
 package com.example.readme_grupo11
 
+import android.media.Image
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.unit.dp
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import com.example.readme_grupo11.ui.theme.README_Grupo11Theme
 import saludo
 
@@ -23,12 +38,59 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //codigo?
-                    val windowSizeClass = obtenerWindowSizeClass()
-                    MainScreen(windowSizeClass)
+                    //codigo
+                    MaterialTheme {
+                        Surface(modifier = Modifier.fillMaxSize()) {
+                            val mensaje = Mensaje("Seba", "Esta wea me tiene aburrido")
+                            setMensaje(mensaje)
+                        }
+                    }
 
                 }
             }
+        }
+    }
+}
+
+data class Mensaje(val autor: String, val mensaje:String)
+
+@Composable
+fun setMensaje(msg: Mensaje){
+
+    Row(modifier = Modifier.padding(all = 8.dp)){
+        Image(
+            painter = painterResource(R.drawable.profile_picture),
+            contentDescription = "Imagen de perfil",
+            modifier = Modifier
+                .size(50.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(
+                "Autor ${msg.autor}",
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface (
+                color = MaterialTheme.colorScheme.primary.copy(alpha = 0.1f), // Fondo suave
+                shape = MaterialTheme.shapes.medium, // Bordes redondeados
+                modifier = Modifier.padding(all = 2.dp)
+            ){
+                Text(
+                    "Autor ${msg.mensaje}",
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+
+
+
         }
     }
 }
