d1641f34d8d2464cf89a4a97f2f19e663a17637e
Sebastian Pino <se.pinob@duocuc.cl>
2025-11-27 17:27:58 -0300

    Proyecto con permisos para red local. Deberia estar listo

--- app/src/main/java/com/example/readme_grupo11/api/RetrofitClient.kt
+++ app/src/main/java/com/example/readme_grupo11/api/RetrofitClient.kt
@@ -5,7 +5,10 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     // URL base, apuntando a la IP especial del emulador y al prefijo /api/
-    private const val BASE_URL = "http://10.0.2.2:8080/api/"
+    private const val BASE_URL = "http://192.168.100.55:8080/api/"
+
+    // URL para MÓVIL FÍSICO (DEBES USAR ESTA y reemplazar la IP)
+    // private const val BASE_URL = "http://[TU_IP_DE_PC]:8080/api/"
 
     val instance: ApiService by lazy {
         val retrofit = Retrofit.Builder()
